2d5f748694b6db1950c1156b5bb09de2b5a18438
Maxtadont <tusaeff@gmail.com>
2020-10-06 23:27:54 +0400

    feat: solve morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const space = '**********';
+    let Symbols = [];
+    let Strings = expr.padEnd(Math.ceil(expr.length/10)*10,'00').split(space);
+    let result = '';
+    for (let i = 0; i < Strings.length; i++) {
+        Symbols.push({
+            bit: [], 
+            morse: [], 
+            char: ''
+        });
+
+        for (let j = 0; j < Strings[i].length / 10; j++) {
+            Symbols[i].bit[j] = Strings[i].substr(j*10, 10);
+            Symbols[i].morse[j] = '';
+        
+            for (z = 0; z < Symbols[i].bit[j].length/2; z++) {
+                Symbols[i].morse[j] += decodeBinToMorse(Symbols[i].bit[j].substr(z*2, 2)); 
+            }
+            
+            Symbols[i].char += MORSE_TABLE[Symbols[i].morse[j]];
+        }
+        result += Symbols[i].char + ' ';
+    }
+
+        return result.trim();
+}
+
+function decodeBinToMorse(str) {
+    switch (str) {
+        case '10': return '.';
+        case '11': return '-';
+        default: return '';
+    }
 }
 
 module.exports = {
